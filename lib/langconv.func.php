f00376dfd2c4e7d31afd484a2f98173383d0a1f2
WuJie <wujie@digiwin.biz>
2017-08-22 15:29:14 +0800

    添加浏览器转换简繁转换功能

--- lib/langconv.func.php
+++ lib/langconv.func.php
@@ -11,4 +11,10 @@ function convertlange($content,   $lange='zh-tw')
     return	MediaWikiZhConverter::convert($content,$lange);
 }
 
-/*判断字符串中是否包含繁体*/
+
+
+/*判断字符串中是否包含繁体尽可能判断 */
+function stristraditioncal($str)
+{
+	return iconv('UTF-8','GB2312',$str)===false?true:false;
+}
